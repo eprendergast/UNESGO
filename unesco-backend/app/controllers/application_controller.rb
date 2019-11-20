2fe4a447a574801ec84385b1c40d62e75143a6aa
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-11-20 16:58:58 +0000

    user authentication done

--- unesco-backend/app/controllers/application_controller.rb
+++ unesco-backend/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+    def get_current_user
+        user_id = decode_token['id']
+        User.find_by(id: user_id)
+    end
+
+    def issue_token(data)
+            JWT.encode(data, secret)
+    end
+
+    def decode_token
+        begin
+            JWT.decode(get_token, secret).first
+        rescue
+            {}
+        end
+    end
+
+    def get_token
+        request.headers['Authorization']
+    end
+
+    def secret
+        ENV["SECRET"]
+    end
+
 end
