5c4d4df329ceee0a5674c9b2f697c3d661000f8e
Elizabeth Prendergast <elizabeth.prendergast@gmail.com>
2019-12-03 11:34:42 +0000

    can handle spaces in tag names

--- unesco-frontend/src/modals/SignUpModal.js
+++ unesco-frontend/src/modals/SignUpModal.js
@@ -2,20 +2,16 @@ import React from 'react'
 import { Modal } from 'semantic-ui-react'
 import SignUpForm from '../components/SignUpForm'
 
-const SignUpModal = ({signup, signin}) => (
-  <Modal trigger={<button className="navbar-button">
-  Sign up
-</button>}>
-                
+const SignUpModal = ({ signup, signin }) => (
+  <Modal closeIcon trigger={<button className='navbar-button'>Sign up</button>}>
     <Modal.Header>Welcome to UNESGO</Modal.Header>
-    
-    <Modal.Content>
-        <SignUpForm signin={signin} signup={signup}/>
-        <Modal.Description>
-          <p>Already have an account? Log in </p>
-        </Modal.Description>
-  </Modal.Content>
 
+    <Modal.Content>
+      <SignUpForm signin={signin} signup={signup} />
+      <Modal.Description>
+        <p>Already have an account? Log in </p>
+      </Modal.Description>
+    </Modal.Content>
   </Modal>
 )
 
